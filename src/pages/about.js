fbe06251f201b811cb2ef45b50c75695dca7c0f0
Duy Bui <halloduy@gmail.com>
2020-10-21 14:50:00 +0200

    about page updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,8 @@ const AboutPage = ({data, location }) => {
     >
     <div class="about-wrap">
       <div class="about-content">
-        <p>Duy Bui (b. 1994 and based in Switzerland) works as a designer and artist. As an interaction designer by training he is using design as a tool of research and inquiry. Influenced by the friction of different cultures, values and societies he is particularly interested in observing and questioning societal, economical and political structures and paradoxes. In his works, he relies on close collaboration with fellow artists, designers and the community. By research and reflection he builds a vocabulary of the topic that he expresses in various ways such as installations, video, photography and mixed-media. Through his works he aims at fostering a basis for discussion and change.</p>
+        <p>Duy Bui (born 1994, based in Switzerland) works in installation, photography, film and fictional media. As an interaction designer by training he is using design as a tool of research and inquiry. Influenced by the friction of different cultures, values and societies he focuses on observing and questioning interpersonal, societal and political structures and paradoxes. Born and raised in Zurich, he is particularly interested in exploring the Asian Identity, its meaning and position. 
+In his works he relies on close collaboration with fellow artists and designers in order to develop a strong and thorough language. By research and reflection he builds a vocabulary of the topic that he expresses in various ways. He has made morphing images, mobile gardens, communicative surfaces, organised workshops and made clementines explode. Through his works he aims at fostering a basis for discussion and change.</p>
       </div>
     </div>
     <div class="about-items-wrap">
@@ -27,21 +28,27 @@ const AboutPage = ({data, location }) => {
       <h2>Exhibitions</h2>
       <ul style={{ listStyle: 'none', }}>
         <li><h3>2019</h3></li>
-        <li>Bonds & Ties 契約與紐帶, Toni-Areal, Zurich, Switzerland</li>
-        <li>HART House, Hong Kong</li>
-        <li>McaM Shanghai, China</li>
-        <li>Design Festival Bern, Switzerland</li>
-        <li>FOGO, Zurich, Switzerland</li>
-        <li>Punkt, Toni-Areal, Zurich, Switzerland</li>
+        <li><i>Bonds & Ties 契約與紐帶</i>, Toni-Areal, Zurich, Switzerland</li>
+        <li><i>Improv Workshop</i>, HART House, Hong Kong</li>
+        <li><i>Tear Up / See You Again</i>, McaM Shanghai, China</li>
+        <li><i>clementine.lab</i>, Design Festival Bern, Switzerland</li>
+        <li><i>Pop-up Kitchen</i>, FOGO, Zurich, Switzerland</li>
+        <li><i>Punkt</i>, Toni-Areal, Zurich, Switzerland</li>
         <li><h3>2018</h3></li>
-        <li>Zauberwald, Lenzerheide, Switzerland</li>
+        <li><i>Zauberwald Lenzerheide</i>, Lenzerheide, Switzerland</li>
       </ul>
       <h2>Screenings</h2>
       <ul style={{ listStyle: 'none', }}>
         <li><h3>2020</h3></li>
-        <li>Isle of Tears, Centre for Community Cultural Development, Hong Kong</li>
+        <li><i>Isle of Tears</i>, Lausanne Underground Film & Music Festival, Lausanne, Switzerland</li>
+        <li><i>Isle of Tears</i>, Centre for Community Cultural Development, Hong Kong</li>
         <li><h3>2019</h3></li>
-        <li>Isle of Tears, Kino Toni, Zurich, Switzerland</li>
+        <li><i>Isle of Tears</i>, Kino Toni, Zurich, Switzerland</li>
+      </ul>
+      <h2>Award</h2>
+      <ul style={{ listStyle: 'none', }}>
+        <li><h3>2020</h3></li>
+        <li><i>Best Short Film - Documentation (Isle of Tears)</i>, International Competition,  Lausanne Underground Film & Music Festival, Lausanne, Switzerland</li>
       </ul>
       </div>
       <div class="about-items">
